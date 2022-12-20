520863c7ff4d8e39c1defc04b34c222fc7293d80
roman <romanze1706@gmail.com>
2022-12-20 15:05:00 +0300

    Метод цикла повторений задач

--- src/Repeatability.java
+++ src/Repeatability.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public enum Repeatability {
 
 
@@ -15,5 +17,20 @@ public enum Repeatability {
 
     public String getNameR() {
         return nameR;
+    }
+
+    public static void checkRepeat(Repeatability repeatability) {
+        if (repeatability == SINGLE) {
+            System.out.println("Повторяемость однократная");
+        }
+        if (repeatability == DAILY) {
+            System.out.println("Следующее повторение - " + LocalDate.now().plusDays(1));
+        } if (repeatability == WEEKLY) {
+            System.out.println("Следующее повторение - " + LocalDate.now().plusDays(7));
+        } if (repeatability == MONTHLY) {
+            System.out.println("Следующее повторение - " + LocalDate.now().plusMonths(1));
+        } if (repeatability == ANNUAL) {
+            System.out.println("Следующее повторение - " + LocalDate.now().plusYears(1));
+        }
     }
 }
