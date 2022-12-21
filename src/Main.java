af127e1a97ebe5fc9deb479d02e4c2f936bcbc85
roman <romanze1706@gmail.com>
2022-12-21 15:43:59 +0300

    Допилил метод создания, только я не знаю, куда он создаёт)))) И создаёт ли вообще.

--- src/Main.java
+++ src/Main.java
@@ -18,53 +18,54 @@ public class Main {
         try (Scanner scanner = new Scanner(System.in)) {
             label:
             while (true) {
-                printMenu();
-                System.out.print("Выберите пункт меню: ");
-                JOptionPane.showInputDialog(null,
+
+
+                int menu = Integer.parseInt(JOptionPane.showInputDialog(null,
 
                         "1. Добавить задачу" + '\n' +
                                 "2. Удалить задачу" + '\n' +
                                 "3. Получить задачу на указанный день" + '\n' +
-                                "0. Выход");
-                if (jOptionPane.getWantsInput()) {
-                    int menu = Integer.parseInt(JOptionPane.showInputDialog(null,
-
-                            "1. Добавить задачу" + '\n' +
-                                    "2. Удалить задачу" + '\n' +
-                                    "3. Получить задачу на указанный день" + '\n' +
-                                    "0. Выход"));;
-                    switch (menu) {
-                        case 1:
-                            inputTask();
-                            break;
-                        case 2:
-                            DailyPlanner.delete(planer);
-                            break;
-                        case 3:
-                            DailyPlanner.getDailyPlan(planer);
-                            break;
-                        case 0:
-                            break label;
-                    }
-                } else {
-                    scanner.next();
-                    System.out.println("Выберите пункт меню из списка!");
+                                "0. Выход"));
+                ;
+                switch (menu) {
+                    case 1:
+                        inputTask(planer);
+                        break;
+                    case 2:
+                        DailyPlanner.delete(planer);
+                        break;
+                    case 3:
+                        DailyPlanner.getDailyPlan(planer);
+                        break;
+                    case 0:
+                        break label;
                 }
+
             }
         }
     }
 
-    private static void inputTask(Map<DailyPlanner, Integer> planer ) {
+    private static void inputTask(Map<DailyPlanner, Integer> planer) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        planer.put(new DailyPlanner(JOptionPane.showInputDialog(null, "Введите название"),
+                        JOptionPane.showInputDialog(null, "Введите описание задачи"),
+                        Type.WORKED,
+                        Repeatability.SINGLE,
+                        LocalDate.parse(JOptionPane.showInputDialog(null,
+                                "Введите дату"), dateTimeFormatter)),
+                Integer.valueOf(JOptionPane.showInputDialog(null, "Введите номер задания")));
+        JOptionPane.showMessageDialog(null,"Задание успешео добавлено в ваш календарь" +
+                "");
 
     }
 
     private static void printMenu() {
         System.out.println(
 
-                        "1. Добавить задачу" + '\n' +
-                         "2. Удалить задачу" + '\n' +
-                         "3. Получить задачу на указанный день" + '\n' +
-                         "0. Выход"
+                "1. Добавить задачу" + '\n' +
+                        "2. Удалить задачу" + '\n' +
+                        "3. Получить задачу на указанный день" + '\n' +
+                        "0. Выход"
 
         );
     }
